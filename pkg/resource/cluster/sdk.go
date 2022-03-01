92362d2690da1e4cddb77b831cd98be4aa926b3d
Nicholas Thomson <nithomso@amazon.com>
2022-03-01 11:56:02 -0800

    Add check for nodegroups still attached

--- pkg/resource/cluster/sdk.go
+++ pkg/resource/cluster/sdk.go
@@ -706,6 +706,12 @@ func (rm *resourceManager) sdkDelete(
 	if clusterDeleting(r) {
 		return r, requeueWaitWhileDeleting
 	}
+	inUse, err := rm.clusterInUse(ctx, r)
+	if err != nil {
+		return nil, err
+	} else if inUse {
+		return r, requeueWaitWhileInUse
+	}
 
 	input, err := rm.newDeleteRequestPayload(r)
 	if err != nil {
