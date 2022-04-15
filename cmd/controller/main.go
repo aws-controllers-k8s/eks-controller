7752115d4f0efe80301c377611f1daa9cc16b3a8
ci-robot <ci-robot@k8s.io>
2022-04-15 17:47:05 +0000

    Update to ACK runtime `v0.18.4`, code-generator `v0.18.4`

--- cmd/controller/main.go
+++ cmd/controller/main.go
@@ -18,6 +18,7 @@ package main
 import (
 	"os"
 
+	ackv1alpha1 "github.com/aws-controllers-k8s/runtime/apis/core/v1alpha1"
 	ackcfg "github.com/aws-controllers-k8s/runtime/pkg/config"
 	ackrt "github.com/aws-controllers-k8s/runtime/pkg/runtime"
 	ackrtutil "github.com/aws-controllers-k8s/runtime/pkg/util"
@@ -31,12 +32,13 @@ import (
 
 	svctypes "github.com/aws-controllers-k8s/eks-controller/apis/v1alpha1"
 	svcresource "github.com/aws-controllers-k8s/eks-controller/pkg/resource"
-	ackv1alpha1 "github.com/aws-controllers-k8s/runtime/apis/core/v1alpha1"
 
 	_ "github.com/aws-controllers-k8s/eks-controller/pkg/resource/addon"
 	_ "github.com/aws-controllers-k8s/eks-controller/pkg/resource/cluster"
 	_ "github.com/aws-controllers-k8s/eks-controller/pkg/resource/fargate_profile"
 	_ "github.com/aws-controllers-k8s/eks-controller/pkg/resource/nodegroup"
+
+	"github.com/aws-controllers-k8s/eks-controller/pkg/version"
 )
 
 var (
@@ -102,7 +104,11 @@ func main() {
 	)
 	sc := ackrt.NewServiceController(
 		awsServiceAlias, awsServiceAPIGroup, awsServiceEndpointsID,
-		ackrt.VersionInfo{}, // TODO: populate version info
+		ackrt.VersionInfo{
+			version.GitCommit,
+			version.GitVersion,
+			version.BuildDate,
+		},
 	).WithLogger(
 		ctrlrt.Log,
 	).WithResourceManagerFactories(
