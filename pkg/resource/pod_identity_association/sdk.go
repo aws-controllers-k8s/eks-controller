e3045cdfc83ce0cf54ab82432788059940c0f8a1
Amine Hilaly <hilalyamine@gmail.com>
2024-02-02 19:11:47 -0600

    Correctly retrieve the ARN for `PodIdentityAssociation`
    In the previous patch, we added tag update support to all EKS resources.
    In the process, we also began populating the PIA resource ARN in the
    `ackResourcemetadata` field. The tag update logic, assumed that all
    resouces would have an ARN in the `ackResourceMetadata`, which is not
    the case for old resources (created by an eks-controller < 1.2.2).
    
    This patch correctly retrieves the ARN from the appropriate location.
    All old resources will have their ARN directly popualted in
    `ackResourceMetadata` once the controller reconciles.
    
    This patch also publishes `v1.2.3` release manifests
    
    Signed-off-by: Amine Hilaly <hilalyamine@gmail.com>

--- pkg/resource/pod_identity_association/sdk.go
+++ pkg/resource/pod_identity_association/sdk.go
@@ -313,9 +313,17 @@ func (rm *resourceManager) sdkUpdate(
 		exit(err)
 	}()
 	if delta.DifferentAt("Spec.Tags") {
+		// TODO(a-hilaly) we need to switch to "ONLY" using the ARN from the ackResourceMetadata
+		// in the future.
+		resourceARN := ""
+		if desired.ko.Status.ACKResourceMetadata.ARN != nil {
+			resourceARN = *latest.ko.Status.AssociationARN
+		} else if desired.ko.Status.AssociationARN != nil {
+			resourceARN = string(*latest.ko.Status.ACKResourceMetadata.ARN)
+		}
 		err := syncTags(
 			ctx, rm.sdkapi, rm.metrics,
-			string(*desired.ko.Status.ACKResourceMetadata.ARN),
+			resourceARN,
 			desired.ko.Spec.Tags, latest.ko.Spec.Tags,
 		)
 		if err != nil {
