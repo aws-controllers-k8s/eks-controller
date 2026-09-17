b84b6690ec2d2404a1eed97b8ce99d6c8acb758b
ci-robot <ci-robot@k8s.io>
2026-09-17 23:51:05 +0000

    Update to ACK runtime `v0.64.0`, code-generator `v0.64.0`

--- pkg/version/version.go
+++ pkg/version/version.go
@@ -27,6 +27,6 @@ var (
 // that generated this controller's code. They are baked in at code-generation
 // time and are immutable for the life of the generated code.
 const (
-	ACKGenerateVersion   = "v0.63.0-8-g3da9ce2"
-	ACKGenerateBuildDate = "2026-09-09T17:47:28Z"
+	ACKGenerateVersion   = "v0.64.0"
+	ACKGenerateBuildDate = "2026-09-17T23:50:55Z"
 )
